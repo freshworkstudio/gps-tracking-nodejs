51c37009ca7b4b38bd532699aecda45b2a8e5f42
Gonzalo De Spirito <gonzunigad@gmail.com>
2017-10-24 23:12:29 -0300

    [Refactoring] Refactor some code and fix coding standard

---
+++ examples/simple.js
@@ -0,0 +1,33 @@
+//var gps = require("gps-tracking");
+var gps = require('../index');
+
+var options = {
+  debug: true,
+  port: 8090,
+  device_adapter: 'TK103'
+}
+
+var server = gps.server(options, function (device, connection) {
+
+  device.on('login_request', function (device_id, msg_parts) {
+
+    // Some devices sends a login request before transmitting their position
+    // Do some stuff before authenticate the device...
+
+    // Accept the login request. You can set false to reject the device.
+    this.login_authorized(true)
+
+  })
+
+  //PING -> When the gps sends their position
+  device.on('ping', function (data) {
+
+    //After the ping is received, but before the data is saved
+    //console.log(data);
+    return data
+
+  });
+
+});
+
+server.setDebug(true);
